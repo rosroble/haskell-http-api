bba6f93b55875d6f465764ef064bc3148ad3c2f0
Boris Poloshkov <bprnd2@yandex.ru>
2024-01-19 15:09:38 +0300

    try use stack-cache, rm unused imports

--- src/Docs.hs
+++ src/Docs.hs
@@ -4,21 +4,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
-module Docs where
+module Docs () where
 
 -- import Network.HTTP.Types
 
 -- import Web.FormUrlEncoded(FromForm(..), ToForm(..))
 
-import ApiType (GetRequest (..), KVAPI, KVEntry (..), KeyValueType (..))
-import Data.ByteString.Lazy (ByteString)
-import Data.Proxy
-import Data.Text.Lazy (pack)
-import Data.Text.Lazy.Encoding (encodeUtf8)
-import Network.Wai
-import Servant.API
+import ApiType (GetRequest (..), KVEntry (..), KeyValueType (..))
 import Servant.Docs
-import Servant.Server
 
 instance ToSample KeyValueType where
   toSamples _ = samples [KVString "SomeString", KVInteger 42]
